5e6113088af905e261c862d52ef129994d2849a7
Paige Niedringhaus <paigen11@gmail.com>
2022-06-10 13:59:52 -0400

    first commit showing how to use notecarrier f with huzzah32 mcu and note arduino lib

---
+++ NotecarrierFSketch.ino
@@ -0,0 +1,52 @@
+#include <Notecard.h>
+#include <NotecardPseudoSensor.h>
+using namespace blues;
+
+#define usbSerial Serial
+#define productUID "com.blues.pniedringhaus:huzzah32_notecarrier_a"
+
+Notecard notecard;
+NotecardPseudoSensor sensor(notecard);
+
+void setup()
+{
+  delay(2500);
+  usbSerial.begin(115200);
+
+  notecard.begin();
+  notecard.setDebugOutputStream(usbSerial);
+
+  J *req = notecard.newRequest("hub.set");
+  JAddStringToObject(req, "product", productUID);
+  JAddStringToObject(req, "mode", "continuous");
+  notecard.sendRequest(req);
+}
+
+void loop()
+{
+  float temperature = sensor.temp();
+  float humidity = sensor.humidity();
+
+  usbSerial.print("Temperature = ");
+  usbSerial.print(temperature);
+  usbSerial.println(" *C");
+  usbSerial.print("Humidity = ");
+  usbSerial.print(humidity);
+  usbSerial.println(" %");
+
+  delay(15000);
+
+  J *req = notecard.newRequest("note.add");
+  if (req != NULL)
+  {
+    JAddStringToObject(req, "file", "sensors.qo");
+    JAddBoolToObject(req, "sync", true);
+    J *body = JAddObjectToObject(req, "body");
+    if (body)
+    {
+      JAddNumberToObject(body, "temp", temperature);
+      JAddNumberToObject(body, "humidity", humidity);
+    }
+    notecard.sendRequest(req);
+  }
+}
